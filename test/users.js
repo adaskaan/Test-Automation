37d20f7945ebcd0badb05f867499fd01798314ba
Ömer Kaan Adaş <adaskaan@gmail.com>
2022-11-29 14:59:54 +0300

    init

---
+++ test/users.js
@@ -0,0 +1,33 @@
+import { describe } from "mocha";
+import supertest from "supertest";
+const request = supertest('https://gorest.co.in/public/');
+
+import { expect } from "chai";
+
+const TOKEN ='ad9727c2064575a2a2498c73021f3aa2b5076348a1ce6b5fcb3edb068c5aa544'
+describe ('Users',()=>{
+    it('GET /users',(done)=>{
+        /*
+        request
+        .get('v2/users?access-token=${TOKEN}').end((err,res)=>{
+            //console.log(res.body);
+            expect(res.body).to.not.be.empty;
+            done();
+        });
+         */
+       return request
+        .get('v2/users?access-token=${TOKEN}').then((res)=>{
+           // console.log(res.body);
+            expect(res.body).to.not.be.empty;
+        });
+    });
+    /*
+    it('GET /users/:id',()=>{
+        return request
+        .get('v2/users/1?access-token=${TOKEN}').then((res)=>{
+           // console.log(res.body);
+            expect(res.body).to.not.be.empty;
+        });
+    });
+    */
+});
